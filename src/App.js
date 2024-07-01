fb6a76d9e86d7c1a1bc2aae2a02a28d9217f5a86
Tianzi Cui <cuitianzi0@gmail.com>
2024-06-30 22:29:39 -0600

    useMovies as a custom hook

--- src/App.js
+++ src/App.js
@@ -1,65 +1,25 @@
 import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
+import { useMovies } from "./useMovie";
 
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
 
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
+
+
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "300c144";
 
+  const KEY = "300c144";
 //lay out component
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [isLoading, setIsLoading] = useState(true);
-  const [errMessage, setErrMessage] = useState("");
   const [query, setQuery] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+
+  //using custom hook
+  //destructure the return value 
+  const{movies, isLoading, errMessage}=useMovies(query, handleCloseMovie);
+  
   //the initial state can be a callback function
   //we can use this callback function to read the data back from local storage
   //the function should be a pure function without any argument
@@ -103,54 +63,6 @@ export default function App() {
   );
 
   //read the data back as soon as the component mounts
-
-  useEffect(
-    function () {
-      //a native web API
-      const controller = new AbortController();
-
-      //the definition of asyn function
-      async function fetchMovies() {
-        try {
-          setIsLoading(true);
-          setErrMessage("");
-          const res = await fetch(
-            `http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}`,
-            { signal: controller.signal }
-          );
-
-          if (!res.ok) throw new Error("Something wrong with fetching movies");
-
-          const data = await res.json();
-          if (data.Response === "False") throw new Error("Movie not Found");
-
-          setMovies(data.Search);
-        } catch (err) {
-          //if the name of error is AbortError then no need to set the message and show it up
-          if (err.name !== "AbortError") {
-            setErrMessage(err.message);
-          }
-        } finally {
-          setIsLoading(false);
-        }
-      }
-      //calling the function
-      //if the query is empty, will not fetch the data
-      if (query.length < 3) {
-        setMovies([]);
-        setErrMessage("");
-        setIsLoading(false);
-        return;
-      }
-      //before fetch a new movie, close the current expanded movie detail
-      handleCloseMovie();
-      fetchMovies();
-      return function () {
-        controller.abort();
-      };
-    },
-    [query]
-  );
 
   return (
     <>
@@ -204,22 +116,25 @@ function Search({ query, setQuery }) {
   //1. create a ref
   const inputEl = useRef(null);
 
-  useEffect(function () {
-    //we place it into callback function so that we can clean it later
-    function callback(e) {
-      //we don't want to set the search bar empty when it is alredy focused
-      if(document.activeElement === inputEl.current) return;
-      if (e.code === "Enter") {
-        //inputEl.current is the DOM
-        inputEl.current.focus();
-        setQuery("");
+  useEffect(
+    function () {
+      //we place it into callback function so that we can clean it later
+      function callback(e) {
+        //we don't want to set the search bar empty when it is alredy focused
+        if (document.activeElement === inputEl.current) return;
+        if (e.code === "Enter") {
+          //inputEl.current is the DOM
+          inputEl.current.focus();
+          setQuery("");
+        }
       }
-    }
-    document.addEventListener("keydown", callback);
-    return () => document.removeEventListener("keydown", callback);
-    //dependency array: 1. props and states that will be updated
-    //2.  the outside function or variable using in useEffect
-  }, [setQuery]);
+      document.addEventListener("keydown", callback);
+      return () => document.removeEventListener("keydown", callback);
+      //dependency array: 1. props and states that will be updated
+      //2.  the outside function or variable using in useEffect
+    },
+    [setQuery]
+  );
 
   return (
     <input
